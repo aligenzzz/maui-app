efa4f102dbbc9ff447714b18261a0e5303e5e5b3
aligenzzz <alina_bybko@mail.ru>
2023-05-11 12:57:46 +0300

    initial project

---
+++ 153501_Bybko/153501_Bybko.UI/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace _153501_Bybko.UI;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ 153501_Bybko/153501_Bybko.UI/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace _153501_Bybko.UI;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
